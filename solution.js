5a6592b957693b4bf16d2b72157d4b5323c945f8
Glitch (hello-webpage) <none>
2020-07-13 17:36:42 +0000

    🎲👵 Checkpoint ./script.js:15299714/811 ./solution.js:15299714/1035 ./index.html:15299714/30

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
-/* global createCanvas, colorMode, HSB, background, random, width, height, ellipse, mouseX, mouseY, text */
+/* global collideCircleCircle, createCanvas, colorMode, HSB, background, random, width, height, ellipse, mouseX, mouseY, text */
 
 let brushHue, backgroundColor, coinX, coinY, score, time, gameIsOver, hit;
 
@@ -10,6 +10,7 @@ function setup() {
   backgroundColor = 95;
   coinX = random(width);
   coinY = random(height);
+  score = 0;
   time = 1000;
   gameIsOver = false;
 }
@@ -18,13 +19,29 @@ function draw() {
   background(backgroundColor);
   ellipse(coinX, coinY, 20);
   ellipse(mouseX, mouseY, 20);
+  text(`Your score is ${score}`, 20, 20);
   text(`Time remaining: ${time}`, 20, 40);
+  handleTime();
+
+  hit = collideCircleCircle(coinX, coinY, 20, mouseX, mouseY, 20);
+  if (hit) {
+    handleCollision();
+  }
 }
 
 function handleCollision() {
-  // We'll write code for what happens if your character hits a coin.
+  if (!gameIsOver) {
+    coinY = random(height);
+    coinX = random(width);
+    score += 1;
+  }
 }
 
 function handleTime() {
-  // We'll write code to handle the time.
+  if (time > 0) {
+    time -= 1;
+  } else {
+    gameIsOver = true;
+    text(`Game over`, 20, 60);
+  }
 }
