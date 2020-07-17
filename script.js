2088d72085883b8454e566eedeb9ac851b8dc98c
Glitch (hello-webpage) <none>
2020-07-17 19:52:38 +0000

    🐪🏖 Checkpoint ./script.js:15299714/2192 ./solution.js:15299714/1851 ./index.html:15299714/41

--- script.js
+++ script.js
@@ -1,72 +1,27 @@
-/* global RainDrop, collideLineCircle, rect, createCanvas, colorMode, HSB, background, height, width, noStroke, fill, random, ellipse */
+/* global createCanvas, colorMode, HSB, background, ellipse, random, width, height */
 
-let drops, bladesOfGrass, spaceBetweenGrass;
+let backgroundColor, spherePosition, rectPosition
 
 function setup() {
-  createCanvas(500, 500);
-  colorMode(HSB);
-
-  drops = [];
-  for (let i = 0; i < 10; i++) {
-    drops.push(new RainDrop());
+  // Canvas & color settings
+  createCanvas(500, 400);
+  colorMode(HSB, 360, 100, 100);
+  backgroundColor = 95;
+  // This variable contains a JSON object
+  spherePosition = {
+    "x": 100,
+    "y": 100
   }
-
-  spaceBetweenGrass = 10;
-  bladesOfGrass = [];
-  for (let i = 1; i < width / spaceBetweenGrass; i++)
-    bladesOfGrass.push(new Grass(i * spaceBetweenGrass));
 }
 
 function draw() {
-  background(0, 0, 95);
-
-  for (let i = 0; i < drops.length; i++) {
-    drops[i].drip();
-    drops[i].show();
-  }
-
-  for (let j = 0; j < bladesOfGrass.length; j++) {
-    if (bladesOfGrass[j].isHitByWater(drops)) {
-      bladesOfGrass[j].grow();
-    }
-    bladesOfGrass[j].show();
-  }
+  background(backgroundColor);
+  ellipse(spherePosition.x, spherePosition.y, 20, 20);
+  // rect(rectPosition.x, rectPosition.y, 20, 20);
+  // line(spherePosition.x, spherePosition.y, rectPosition.x, rectPosition.y);
 }
 
-class Grass {
-  constructor(x) {
-    this.x = x;
-    this.bladeHeight = 5;
-    this.bladeWidth = 5;
-  }
-
-  isHitByWater(drops) {
-    for (let i = 0; i < drops.length; i++) {
-      if (
-        // Only count as hit if the water hits the ground
-        collideLineCircle(
-          this.x,
-          height,
-          this.x + this.bladeWidth,
-          height,
-          drops[i].x,
-          drops[i].y,
-          drops[i].d
-        )
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  grow() {
-    this.bladeHeight += 5;
-  }
-
-  show() {
-    noStroke();
-    fill(130, 100, 75);
-    rect(this.x, height - this.bladeHeight, this.bladeWidth, this.bladeHeight);
-  }
+function mousePressed() {
+  spherePosition.x = random(width);
+  spherePosition.y = random(height);
 }
