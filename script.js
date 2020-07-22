b1cc7dba38daa1442183330d359f29620e5a5b5a
Glitch (hello-webpage) <none>
2020-07-22 19:49:02 +0000

    🚲👩‍❤️‍💋‍👩 Checkpoint ./script.js:18122837/508 ./index.html:18122837/58:18530887/358 ./.glitch-assets:18122837/1340

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-/*global loadSound, soundFormats, createCanvas, colorMode, background, fill, HSB*/
+/*global mouseIsPressed, text, canvasPressed, loadSound, soundFormats, createCanvas, colorMode, background, fill, HSB*/
 
 let mySound;
 
 function preload(){
   soundFormats('mp3');
-  mySound = loadSound('assets/[MP3FY] Wilhelm Scream sound effect.mp3');
+  mySound = loadSound('assets/songExample.mp3');
   
 }
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
-  
 }
 
 function draw() {
   background(95);
-  mySound.play();
-  
-  
+  if(mouseIsPressed){
+    mySound.play();
+  }
 }
 
 
