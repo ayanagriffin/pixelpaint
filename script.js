761cf562e1b4dcf08404f898a31d3cf6921ba1b9
Glitch (hello-webpage) <none>
2020-07-23 23:03:04 +0000

    🐂📟 Checkpoint ./script.js:18280517/1341

--- script.js
+++ script.js
@@ -19,7 +19,13 @@ function setup() {
   for(let i = 0; i < numRows; i++){
     for(let j = 0; j < numCols; j++){
       blocks.push(new Block(i, j));
+    
     }
+  }
+  
+  for(let i = 0; i < blocks.length; i++){
+    blocks[i].getColors();
+    blocks[i].findAverageColor();
   }
 }
 
@@ -28,9 +34,10 @@ function draw() {
   image(img, 0, 0, imgW, imgH);
   
   for(let i = 0; i < blocks.length; i++){
-    blocks[i].getColors();
     blocks[i].draw();
   }
+  
+  //console.log(blocks[3].finalB);
   
 }
 
@@ -43,9 +50,16 @@ class Block{
     this.startingX = this.row * this.width;
     this.startingY = this.col * this.height;
     this.colors = [];
+    this.totalR = 0;
+    this.totalG = 0;
+    this.totalB = 0;
+    this.finalR = 0;
+    this.finalG = 0;
+    this.finalB = 0;
   }
   
   getColors(){
+  
     for(let i = this.startingX; i < this.width + this.startingX; i++){
       for(let j = this.startingY; j < this.height + this.startingY; j++){
         this.colors.push(get(i, j));
@@ -54,20 +68,26 @@ class Block{
   }
   
   findAverageColor(){
-    let totalR = 0;
-    let totalG = 0;
-    let totalB = 0;
+   
     
     for(let i = 0; i < this.colors.length; i++){
-      totalR += this.colors[0];
-      totalG += this.colors[1];
-      totalB += this.colors[2];
+      this.totalR += this.colors[0];
+      this.totalG += this.colors[1];
+      this.totalB += this.colors[2];
     }
+    
+    this.finalR = this.totalR / this.colors.length;
+    this.finalG = this.totalG / this.colors.length;
+    this.finalB = this.totalB / this.colors.length;
+    
+    //console.log(this.colors.length);
+    //console.log(this.finalR, this.finalG, this.finalB);
+    
   }
   draw(){
-    fill(this.colors[0]);
+    fill(this.finalR, this.finalG, this.finalB);
     rect(this.startingX, this.startingY, this.width, this.height);
-    console.log(this.colors[0]);
+    //console.log(this.colors[0]);
     
   }
   
