b0e9c6021cc1dd648746d02ca3a15d36e1d99db8
Glitch (hello-webpage) <none>
2020-07-23 23:14:57 +0000

    🎾🐉 Checkpoint ./script.js:18280517/5600

--- script.js
+++ script.js
@@ -2,47 +2,51 @@
 
 let img, numRows, numCols, imgW, imgH, blockW, blockH, blocks;
 
-function preload(){
-  img = loadImage("https://cdn.glitch.com/d82135a4-9f9d-4654-a46b-f7f58cdc9d01%2Fimg_lights.jpg?v=1595543508242");
+function preload() {
+  img = loadImage(
+    "https://cdn.glitch.com/d82135a4-9f9d-4654-a46b-f7f58cdc9d01%2Fimg_lights.jpg?v=1595543508242"
+  );
 }
 function setup() {
   createCanvas(400, 400);
   colorMode(RGB, 255, 255, 255);
-  numRows = 2; 
+  numRows = 2;
   numCols = 2;
   imgW = 300;
   imgH = 300;
   blockW = imgW / numCols;
   blockH = imgH / numRows;
   blocks = [];
-  
-  for(let i = 0; i < numRows; i++){
-    for(let j = 0; j < numCols; j++){
+
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
       blocks.push(new Block(i, j));
-    
     }
   }
-  
-  for(let i = 0; i < blocks.length; i++){
+
+  for (let i = 0; i < blocks.length; i++) {
     blocks[i].getColors();
     blocks[i].findAverageColor();
   }
 }
 
 function draw() {
-  background(95);
+  background(100);
   image(img, 0, 0, imgW, imgH);
-  
-  for(let i = 0; i < blocks.length; i++){
+
+  for (let i = 0; i < blocks.length; i++) {
     blocks[i].draw();
+    //blocks[i].test();
   }
-  
-  //console.log(blocks[3].finalB);
+
+  // console.log(blocks[3].totalB);
+  // console.log(blocks[3].colors.length);
+  //console.log(0 / 22500);
   
 }
 
-class Block{
-  constructor(row, col){
+class Block {
+  constructor(row, col) {
     this.row = row;
     this.col = col;
     this.width = blockW;
@@ -57,43 +61,46 @@ class Block{
     this.finalG = 0;
     this.finalB = 0;
   }
-  
-  getColors(){
-  
-    for(let i = this.startingX; i < this.width + this.startingX; i++){
-      for(let j = this.startingY; j < this.height + this.startingY; j++){
+
+  getColors() {
+    for (let i = this.startingX; i < this.width + this.startingX; i++) {
+      for (let j = this.startingY; j < this.height + this.startingY; j++) {
         this.colors.push(get(i, j));
+        
       }
     }
+    
   }
-  
-  findAverageColor(){
-   
+
+  findAverageColor() {
     
-    for(let i = 0; i < this.colors.length; i++){
-      this.totalR += this.colors[0];
-      this.totalG += this.colors[1];
-      this.totalB += this.colors[2];
+    let totalR = 0;
+    let totalB = 0
+    let totalG = 0;
+    for (let i = 0; i < this.colors.length; i++) {
+      totalR += this.colors[i][0];
+      totalG += this.colors[i][1];
+      totalB += this.colors[i][2];
     }
+
     
-    this.finalR = this.totalR / this.colors.length;
-    this.finalG = this.totalG / this.colors.length;
-    this.finalB = this.totalB / this.colors.length;
-    
+    this.finalR = totalR / this.colors.length;
+    this.finalG = totalG / this.colors.length;
+    this.finalB = totalB / this.colors.length;
+
     //console.log(this.colors.length);
     //console.log(this.finalR, this.finalG, this.finalB);
-    
+    //return this.finalB;
   }
-  draw(){
-    fill(this.finalR, this.finalG, this.finalB);
+  draw() {
+    fill(this.finalR, this.finalB, this.finalG);
     rect(this.startingX, this.startingY, this.width, this.height);
     //console.log(this.colors[0]);
-    
   }
   
-  
+  test(){
+    for(let i = 0; i < this.colors.length; i++){
+      console.log(this.colors[i]);
+    }
+  }
 }
-
-
-
-
