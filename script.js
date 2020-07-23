75088e5c814d33cea6e92db46aec5ce308ee930e
Glitch (hello-webpage) <none>
2020-07-23 22:51:48 +0000

    ✍️🐔 Checkpoint ./script.js:18280517/1221

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*global createCanvas, colorMode, HSB, background, image, loadImage*/
+/*global createCanvas, colorMode, HSB, background, image, loadImage, get, fill, rect, RGB*/
 
 let img, numRows, numCols, imgW, imgH, blockW, blockH, blocks;
 
@@ -7,24 +7,29 @@ function preload(){
 }
 function setup() {
   createCanvas(400, 400);
-  colorMode(HSB, 360, 100, 100);
-  numRows = 10; 
-  numCols = 10;
+  colorMode(RGB, 255, 255, 255);
+  numRows = 2; 
+  numCols = 2;
   imgW = 300;
   imgH = 300;
   blockW = imgW / numCols;
   blockH = imgH / numRows;
   blocks = [];
+  
+  for(let i = 0; i < numRows; i++){
+    for(let j = 0; j < numCols; j++){
+      blocks.push(new Block(i, j));
+    }
+  }
 }
 
 function draw() {
   background(95);
   image(img, 0, 0, imgW, imgH);
   
-  for(let i = 0; i < numRows; i++){
-    for(let j = 0; j < numCols; j++){
-      blocks.push(new Block(i, j));
-    }
+  for(let i = 0; i < blocks.length; i++){
+    blocks[i].getColors();
+    blocks[i].draw();
   }
   
 }
@@ -33,14 +38,40 @@ class Block{
   constructor(row, col){
     this.row = row;
     this.col = col;
-    this.startingX = this.row * blockW;
-    this.startingY = this.col * blockH;
+    this.width = blockW;
+    this.height = blockH;
+    this.startingX = this.row * this.width;
+    this.startingY = this.col * this.height;
     this.colors = [];
   }
   
   getColors(){
+    for(let i = this.startingX; i < this.width + this.startingX; i++){
+      for(let j = this.startingY; j < this.height + this.startingY; j++){
+        this.colors.push(get(i, j));
+      }
+    }
+  }
+  
+  findAverageColor(){
+    let totalR = 0;
+    let totalG = 0;
+    let totalB = 0;
+    
+    for(let i = 0; i < this.colors.length; i++){
+      totalR += this.colors[0];
+      totalG += this.colors[1];
+      totalB += this.colors[2];
+    }
+  }
+  draw(){
+    fill(this.colors[0]);
+    rect(this.startingX, this.startingY, this.width, this.height);
+    console.log(this.colors[0]);
     
   }
+  
+  
 }
 
 
