a84e8098484585c51c750b058e4700d5a8f4bd86
Glitch (hello-webpage) <none>
2020-07-23 00:59:01 +0000

    🚀🎾 Checkpoint ./script.js:18280517/512 ./.glitch-assets:18280517/86

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-/*global mouseIsPressed, text, canvasPressed, loadSound, soundFormats, createCanvas, colorMode, background, fill, HSB*/
+/*global createCanvas, colorMode, HSB, background*/
 
-let mySound;
+let variables;
 
-function preload(){
-  soundFormats('mp3');
-  mySound = loadSound('https://cdn.glitch.com/923ded2e-470f-4401-a948-0e2b20cf279c%2FsongExample.mp3?v=1595447316715');
-  
-}
 function setup() {
-  // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
 }
 
 function draw() {
   background(95);
-  if(mouseIsPressed){
-    mySound.play();
-  }
+  
 }
 
 
