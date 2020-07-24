f394f79a1973d3d6d36f10c60dc1421e596b46b1
Glitch (hello-webpage) <none>
2020-07-24 15:26:07 +0000

    🏆🎩 Checkpoint ./imageClass.js:18280517/808 ./index.html:18280517/54

--- imageClass.js
+++ imageClass.js
@@ -1,9 +1,17 @@
 /*global createCanvas, colorMode, HSB, background, image, loadImage, get, abs, fill, rect, RGB, noStroke*/
 // TODO: combine! possibly eliminate setup and draw
 
-class Image {
-  constructor(imgLink) {
-    this.img = loadImage(imgLink);
+
+function setup(){
+  createCanvas(400,400);
+  let img = new Pic();
+  img.getFinalArray();
+  
+  
+}
+class Pic {
+  constructor() {
+    this.img = loadImage("https://cdn.glitch.com/c6a55a91-1fc8-414c-9c30-7b343a077157%2Fdownload.png?v=1595548272909");
     this.numRows = 20;
     this.numCols = 20;
     this.imgW = 300;
@@ -17,6 +25,7 @@ class Image {
     this.cushion = 100;
     image(this.img, 0, 0, this.imgW, this.imgH);
   }
+  
 
   getBlocks() {
     for (let i = 0; i < this.numRows; i++) {
@@ -48,7 +57,7 @@ class Image {
       }
     }
 
-    //this.refactorColors();
+    this.refactorColors();
   }
 
   refactorColors() {
@@ -125,6 +134,11 @@ class Image {
   }
 
   getFinalArray() {
+    
+    this.getBlocks();
+    this.initializeColorVals();
+    this.findBlockColors();
+    console.log(this.colorVals);
     return this.colorVals;
   }
 }
@@ -199,86 +213,86 @@ class Block {
 //   }
 // }
 
-function refactorColors() {
-  for (let i = 0; i < blocks.length; i++) {
-    for (let j = 0; j < blocks[i].length; j++) {
-      if (!blocks[j][i].foundMatch) {
-        //let color = blocks[i].finalColor;
-        numColors++;
-        //blocks[i].foundMatch = true;
-        findMatches(blocks[j][i], numColors);
+// function refactorColors() {
+//   for (let i = 0; i < blocks.length; i++) {
+//     for (let j = 0; j < blocks[i].length; j++) {
+//       if (!blocks[j][i].foundMatch) {
+//         //let color = blocks[i].finalColor;
+//         numColors++;
+//         //blocks[i].foundMatch = true;
+//         findMatches(blocks[j][i], numColors);
 
-        console.log(numColors);
-      }
-    }
-  }
-  // while (numColors < maxColors) {
-  //   let color = blocks[i].finalColor;
-  //   blocks[i].foundMatch = true;
-  //   //findMatches(color, 20);
-  //   numColors++;
-  //   //console.log(color);
-  // }
-}
+//         console.log(numColors);
+//       }
+//     }
+//   }
+//   // while (numColors < maxColors) {
+//   //   let color = blocks[i].finalColor;
+//   //   blocks[i].foundMatch = true;
+//   //   //findMatches(color, 20);
+//   //   numColors++;
+//   //   //console.log(color);
+//   // }
+// }
 
-function findMatches(testBlock, colorVal) {
-  let matches = [];
-  for (let i = 0; i < blocks.length; i++) {
-    for (let j = 0; j < blocks[i].length; j++) {
-      let curBlock = blocks[j][i];
-      if (!curBlock.foundMatch) {
-        //console.log(testBlock.finalR, curBlock.finalR);
+// function findMatches(testBlock, colorVal) {
+//   let matches = [];
+//   for (let i = 0; i < blocks.length; i++) {
+//     for (let j = 0; j < blocks[i].length; j++) {
+//       let curBlock = blocks[j][i];
+//       if (!curBlock.foundMatch) {
+//         //console.log(testBlock.finalR, curBlock.finalR);
 
-        if (
-          abs(testBlock.finalR - curBlock.finalR) < cushion &&
-          abs(testBlock.finalG - curBlock.finalG) < cushion &&
-          abs(testBlock.finalB - curBlock.finalB) < cushion
-        ) {
-          matches.push(curBlock);
-          //curBlock.finalColor = testBlock.finalColor;
-          // curBlock.finalR = testBlock.finalR;
-          // curBlock.finalG = testBlock.finalG;
-          // curBlock.finalB = testBlock.finalB;
-          curBlock.foundMatch = true;
-          colorVals[j][i] = colorVal;
+//         if (
+//           abs(testBlock.finalR - curBlock.finalR) < cushion &&
+//           abs(testBlock.finalG - curBlock.finalG) < cushion &&
+//           abs(testBlock.finalB - curBlock.finalB) < cushion
+//         ) {
+//           matches.push(curBlock);
+//           //curBlock.finalColor = testBlock.finalColor;
+//           // curBlock.finalR = testBlock.finalR;
+//           // curBlock.finalG = testBlock.finalG;
+//           // curBlock.finalB = testBlock.finalB;
+//           curBlock.foundMatch = true;
+//           colorVals[j][i] = colorVal;
 
-          //console.log('match: ', i);
-        } else {
-          // console.log('no match: ', i);
-        }
-      }
-    }
-  }
+//           //console.log('match: ', i);
+//         } else {
+//           // console.log('no match: ', i);
+//         }
+//       }
+//     }
+//   }
 
-  findAverageColor(matches);
-}
+//   findAverageColor(matches);
+// }
 
-function findAverageColor(matches) {
-  let totalR = 0;
-  let totalG = 0;
-  let totalB = 0;
+// function findAverageColor(matches) {
+//   let totalR = 0;
+//   let totalG = 0;
+//   let totalB = 0;
 
-  for (let i = 0; i < matches.length; i++) {
-    let curBlock = matches[i];
-    totalR += curBlock.finalR;
-    totalG += curBlock.finalG;
-    totalB += curBlock.finalB;
-  }
+//   for (let i = 0; i < matches.length; i++) {
+//     let curBlock = matches[i];
+//     totalR += curBlock.finalR;
+//     totalG += curBlock.finalG;
+//     totalB += curBlock.finalB;
+//   }
 
-  let finalR = totalR / matches.length;
-  let finalG = totalG / matches.length;
-  let finalB = totalB / matches.length;
+//   let finalR = totalR / matches.length;
+//   let finalG = totalG / matches.length;
+//   let finalB = totalB / matches.length;
 
-  let finalColor = [finalR, finalG, finalB];
+//   let finalColor = [finalR, finalG, finalB];
 
-  for (let i = 0; i < matches.length; i++) {
-    let curBlock = matches[i];
-    curBlock.finalColor = finalColor;
-  }
-}
+//   for (let i = 0; i < matches.length; i++) {
+//     let curBlock = matches[i];
+//     curBlock.finalColor = finalColor;
+//   }
+// }
 
-// MOST IMPORTANT INFO!! Use this to draw final image for user to color!!!
+// // MOST IMPORTANT INFO!! Use this to draw final image for user to color!!!
 
-function getFinalColorArray() {
-  return colorVals;
-}
+// function getFinalColorArray() {
+//   return colorVals;
+// }
