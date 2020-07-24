2307d9fd7666070f89a939ef11ce6cb19a916d60
Glitch (hello-webpage) <none>
2020-07-24 03:30:37 +0000

    ⏲📅 Checkpoint ./script.js:18280517/7051

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-/*global createCanvas, colorMode, HSB, background, image, loadImage, get, fill, rect, RGB, noStroke*/
+/*global createCanvas, colorMode, HSB, background, image, loadImage, get, abs, fill, rect, RGB, noStroke*/
 
-let img, numRows, numCols, imgW, imgH, blockW, blockH, blocks, finalColors, maxColors, numColors, color1, color2, color3;
+let img,
+  numRows,
+  numCols,
+  imgW,
+  imgH,
+  blockW,
+  blockH,
+  blocks,
+  finalColors,
+  maxColors,
+  numColors,
+  color1,
+  color2,
+  color3, closeness;
 
 function preload() {
   img = loadImage(
@@ -10,8 +23,8 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
   colorMode(RGB, 255);
-  numRows = 5;
-  numCols = 5;
+  numRows = 3;
+  numCols = 3;
   imgW = 300;
   imgH = 300;
   blockW = imgW / numCols;
@@ -26,7 +39,6 @@ function setup() {
       blocks.push(new Block(i, j));
     }
   }
-
 }
 
 function draw() {
@@ -35,12 +47,11 @@ function draw() {
 
   for (let i = 0; i < blocks.length; i++) {
     blocks[i].draw();
-    
+    console.log(blocks[i].finalColor);
   }
 
   getFinalColors();
-  refactorColors(0);
-  
+  refactorColors();
 }
 
 class Block {
@@ -58,22 +69,23 @@ class Block {
     this.finalG = 0;
     this.finalB = 0;
     this.finalColor = [];
+    this.foundMatch = false;
   }
 
   getColors() {
-  // creates and returns an array consisting of the R,G,B,A color values of each pixel in the block
+    // creates and returns an array consisting of the R,G,B,A color values of each pixel in the block
     let colors = [];
     for (let i = this.startingX; i < this.width + this.startingX; i++) {
       for (let j = this.startingY; j < this.height + this.startingY; j++) {
         colors.push(get(i, j));
       }
     }
-    
+
     return colors;
   }
 
   findAverageColor(colors) {
-  // goes through each color value of each pixel in the block and finds the average color
+    // goes through each color value of each pixel in the block and finds the average color
     let totalR = 0;
     let totalG = 0;
     let totalB = 0;
@@ -88,15 +100,13 @@ class Block {
     this.finalB = totalB / colors.length;
 
     this.finalColor = [this.finalR, this.finalG, this.finalB];
-    
-   return this.finalColor;
-   
+
+    return this.finalColor;
   }
   draw() {
     noStroke();
     fill(this.findAverageColor(this.getColors()));
     rect(this.startingX, this.startingY, this.width, this.height);
-    
   }
 }
 
@@ -104,19 +114,39 @@ function getFinalColors() {
   for (let i = 0; i < blocks.length; i++) {
     finalColors.push(blocks[i].finalColor);
   }
-  
 }
 
-function refactorColors(i){
-  while(numColors < maxColors){
-    let color = blocks[i].finalColor;
-    findMatches(color);
-    numColors++;
-    console.log(color);
+function refactorColors() {
+  
+  for(let i = 0; i < blocks.length; i++){
+    if(!blocks[i].foundMatch){
+      let color = blocks[i].finalColor;
+      
+    }
     
   }
+  // while (numColors < maxColors) {
+  //   let color = blocks[i].finalColor;
+  //   blocks[i].foundMatch = true;
+  //   //findMatches(color, 20);
+  //   numColors++;
+  //   //console.log(color);
+  // }
 }
 
-function findMatches(color){
- // for(let i = 0)
+function findMatches(color, closeness) {
+  for (let i = 0; i < blocks.length; i++) {
+    let curBlock = blocks[i];
+    if (!curBlock.foundMatch) {
+      if (
+        abs(color.finalR - curBlock.finalR) < closeness &&
+        abs(color.finalG - curBlock.finalG) < closeness &&
+        abs(color.finalB - curBlock.finalB) < closeness
+      ) {
+        curBlock.finalColor = color;
+        curBlock.foundMatch = true;
+        
+      }
+    }
+  }
 }
