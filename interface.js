078e3bb917225ac3265f395c28a6c1eb4dcc4831
Glitch (hello-webpage) <none>
2020-07-26 04:43:35 +0000

    🎹💄 Checkpoint ./style.css:18280517/9 ./interface.js:18280517/308 ./index.html:18280517/129

---
+++ interface.js
@@ -0,0 +1,19 @@
+async function userUpload(event){
+  
+}
+
+async function newImage(event){
+  console.log("pressed");
+}
+
+async function restart(event){
+  
+}
+
+async function finishImage(event){
+  
+}
+
+async function undo(event){
+  
+}
