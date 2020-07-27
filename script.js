a017125054e0c5a55b9f281640f900473369e24a
Glitch (hello-webpage) <none>
2020-07-27 04:31:53 +0000

    🏐🌉 Checkpoint ./script.js:18280517/952

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*global loadImage, createCanvas, resizeCanvas, background, text*/
+/*global loadImage, createCanvas, resizeCanvas, background, text, windowWidth, windowHeight*/
 
 let imgDimensions = { w: 0, h: 0 };
 let imgUrl, display, imgDimensions2;
@@ -14,6 +14,8 @@ function preload() {
 function setup() {
   createCanvas(TEST_SIZE, TEST_SIZE);
   getDimensions(imgUrl);
+  //console.log(imgDimensions.w);
+  //adjustCanvas();
   
   //console.log(imgDimensions); // logs 0, 0 but if you expand, logs the expected output
   
@@ -21,7 +23,7 @@ function setup() {
 }
 
 
-//updates dimensions and returns a Promise after image finishes loading -- RESEARCH PROMISES
+//updates dimensions and returns a Promise after image finishes loading 
 function getDimensions(url) {
   let image = new Image();
   image.src = url;
@@ -29,18 +31,20 @@ function getDimensions(url) {
     image.onload = function() {
       imgDimensions.w = this.width;
       imgDimensions.h = this.height;
-      adjustCanvas();
+      adjustCanvas(imgDimensions.w, imgDimensions.h);
       //console.log(imgDimensions); //logs expected output 
       resolve();
       
     };
   });
   
- //console.log(imgDimensions);
+ 
   
 }
 
-function adjustCanvas(){
+function adjustCanvas(w, h){
+  //console.log(w, h); //logs expected output
+  
   // trying to adjust the size of the canvas to be the size of the image. 
   //will later adjust the size of the image first to ensure it fits nicely in the canvas (i.e. it is not too tiny or large)
   
@@ -50,10 +54,24 @@ function adjustCanvas(){
   // console.log(newWidth, newHeight);
   // resizeCanvas(newWidth, newHeight);
   
-  console.log(imgDimensions); //logs expected output 
+ // console.log(imgDimensions); //logs expected output 
   //console.log(imgDimensions.w, imgDimensions.h); //logs expected output
   //text(imgDimensions.w, 10, 10); //shows expected value
- // resizeCanvas(imgDimensions.w, imgDimensions.h); // reads it as 0, 0 
+  fitImage();
+  resizeCanvas(w, h); // reads it as 0, 0 
 }
 
 
+function fitImage() {
+  let minPicDim = Math.min(imgDimensions.w, imgDimensions.h);
+  let minWindowDim = Math.min(windowWidth, windowHeight);
+  if (minPicDim == imgDimensions.w) {
+    imgDimensions.w =
+      ((minWindowDim * 2) / 3) * (imgDimensions.w / imgDimensions.h);
+    imgDimensions.h = (minWindowDim * 2) / 3;
+  } else if (minPicDim == imgDimensions.h) {
+    imgDimensions.h =
+      (minWindowDim - 100) * (imgDimensions.h / imgDimensions.w);
+    imgDimensions.w = minWindowDim - 100;
+  }
+}
