74727dad42f2095bfd85ce9df297780e6e4350cd
Glitch (hello-webpage) <none>
2020-07-27 03:23:12 +0000

    🎑🐡 Checkpoint ./test1001.js:18280517/2053

--- test1001.js
+++ test1001.js
@@ -1,30 +1,36 @@
 /*global loadImage*/
 
-let imgUrl, display;
-
 let imgDimensions = { w: 0, h: 0 };
-
-function preload() {
-  // need to preload image for it to function properly
-  imgUrl = "https://cdn.glitch.com/c6a55a91-1fc8-414c-9c30-7b343a077157%2Fdownload.png?v=1595548272909";
-  display = loadImage(imgUrl);
-}
-
-function setup(){
-  getDimensions(imgUrl);
-}
-
-function getDimensions(imgUrl){
-  let image = new Image();
+let imgUrl, display;
   
-  image.onload = function(){
-    console.log("loaded");
-  };
-  image.src = imgUrl;
-  imgDimensions.w = image.width;
-  imgDimensions.h = image.height;
-  console.log(image.src, imgDimensions);
- 
+function preload() {
+  imgUrl =
+    "https://cdn.glitch.com/c6a55a91-1fc8-414c-9c30-7b343a077157%2Fdownload.png?v=1595548272909";
+  display = loadImage(imgUrl);
+  
+}
+
+function setup() {
+  getDimensions(imgUrl);
+  
 
 }
+
+
+//updates dimensions and returns a Promise after image finishes loading -- RESEARCH PROMISES
+function getDimensions(url) {
+  let image = new Image();
+  image.src = url;
+  return new Promise((resolve, reject) => {
+    image.onload = function() {
+      imgDimensions.w = this.width;
+      imgDimensions.h = this.height;
+      
+      
+    };
+  });
+  
+  
+}
+
 
