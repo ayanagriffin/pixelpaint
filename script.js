bef640bfd0df947d825ad253058c404e7f3b8722
Glitch (hello-webpage) <none>
2020-07-30 00:35:30 +0000

    🚇🚴 Checkpoint ./.glitch-assets:18280517/454 ./script.js:18280517/218

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const BLOCK_SIZE = 20;
 
 function preload() {
   imgUrl =
-    "https://cdn.glitch.com/d82135a4-9f9d-4654-a46b-f7f58cdc9d01%2Fmosaic-758754_960_720.webp?v=1595547170968";
+    "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2F2921a2c69e186f704f87435027ba4987.jpg?v=1596068699748";
   display = loadImage(imgUrl);
 }
 
