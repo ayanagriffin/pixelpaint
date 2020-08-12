e77c4317baa737ad39205c7528102c7a484d4694
Glitch (hello-webpage) <none>
2020-08-12 19:23:33 +0000

    😼🐯 Checkpoint ./script.js:18280517/206 ./index.html:18280517/145 ./style.css:18280517/135

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let imgUrl,
 
 function preload() {
   imgUrl =
-    "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2F2921a2c69e186f704f87435027ba4987.jpg?v=1596068699748";
+    "https://cdn.glitch.com/d82135a4-9f9d-4654-a46b-f7f58cdc9d01%2Fimg_lights.jpg?v=1595543508242";
   display = loadImage(imgUrl);
 }
 
