4e38f9829970a3a0936d21b80be4b61cab87d2a7
Glitch (hello-webpage) <none>
2020-08-13 00:16:31 +0000

    👜🌁 Checkpoint ./.glitch-assets:18280517/1363 ./default-pics.js:18280517/673

--- default-pics.js
+++ default-pics.js
@@ -2,5 +2,8 @@ let defaultPics = [
   "https://cdn.glitch.com/d82135a4-9f9d-4654-a46b-f7f58cdc9d01%2Fmosaic-758754_960_720.webp?v=1595547170968",
   "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2FMrtallimage.webp?v=1595864859609",
   "https://cdn.glitch.com/d82135a4-9f9d-4654-a46b-f7f58cdc9d01%2Fimg_lights.jpg?v=1595543508242",
-  "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2F2921a2c69e186f704f87435027ba4987.jpg?v=1596068699748"
+  "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2F2921a2c69e186f704f87435027ba4987.jpg?v=1596068699748",
+  "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2F6-69178_donut-donuts-frames-illustrations-hd-clipart-transparent-donut.jpg?v=1597277231269",
+  "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2F01814_01_sunrichorangesum.jpg?v=1597277230301",
+  "https://cdn.glitch.com/f91fc56a-e988-47d9-bd82-072447cac29f%2Fdownload.jpg?v=1597277229594"
 ];
