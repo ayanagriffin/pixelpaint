13a71c0a105262d4934a14e030154cfbd543334c
Glitch (hello-webpage) <none>
2020-08-14 21:51:17 +0000

    📍📇 Checkpoint ./classes/Block.js:18280517/311

--- classes/Block.js
+++ classes/Block.js
@@ -11,7 +11,6 @@ class Block {
     this.row = row;
     this.col = col;
     this.size = blockSize;
-    this.totalPixels = this.size * this.size;
     this.startingX = this.col * this.size;
     this.startingY = this.row * this.size;
     this.endingX = this.startingX + this.size;
@@ -22,14 +21,15 @@ class Block {
   }
 
   getAverageColor() {
-    for (let i = this.startingX; i < this.endingX; i++) {
-      for (let j = this.startingY; j < this.endingY; j++) {
-        
+    let totalPixels = 0;
+    for (let i = this.startingX; i < this.endingX; i+=4) {
+      for (let j = this.startingY; j < this.endingY; j+=3) {
+        let curPixel = display.get(i, j);
         // checks for transparent bkg; if transparent, set it to white instead
-        if (display.get(i, j)[3] === 255) {
-          this.colorVals[0] += display.get(i, j)[0];
-          this.colorVals[1] += display.get(i, j)[1];
-          this.colorVals[2] += display.get(i, j)[2];
+        if (curPixel[3] === 255) {
+          this.colorVals[0] += curPixel[0];
+          this.colorVals[1] += curPixel[1];
+          this.colorVals[2] += curPixel[2];
         }else{
           this.colorVals[0] += 255;
           this.colorVals[1] += 255;
@@ -39,12 +39,14 @@ class Block {
           /* PROBLEM: going way too slow and adding too many when slider is adjusted -- if blockSize is 20,
           should only log "color values added" 400x per block, but is doing way more than this when slider is adjusted
           for some reason*/
+        
+        totalPixels++;
       }
     }
 
-    this.colorVals[0] = floor(this.colorVals[0] / this.totalPixels);
-    this.colorVals[1] = floor(this.colorVals[1] / this.totalPixels);
-    this.colorVals[2] = floor(this.colorVals[2] / this.totalPixels);
+    this.colorVals[0] = floor(this.colorVals[0] / totalPixels);
+    this.colorVals[1] = floor(this.colorVals[1] / totalPixels);
+    this.colorVals[2] = floor(this.colorVals[2] / totalPixels);
 
     return this.colorVals;
     
